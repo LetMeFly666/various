c3ad13aef89987b3a9acfb655e477857e85e7ddd
LetMeFly666 <814114971@qq.com>
2021-01-21 18:41:48 +0800

    增加

--- autorun/BUCTdaka.py
+++ autorun/BUCTdaka.py
@@ -5,7 +5,22 @@ import os
 def getdate():
     return (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime("%Y%m%d")
 
-headers = {
+
+if __name__ == '__main__':
+    ADDRESS_BUCTDAKA = os.environ["ADDRESS_BUCTDAKA"]
+    AREA_BUCTDAKA = os.environ["AREA_BUCTDAKA"]
+    CITY_BUCTDAKA = os.environ["CITY_BUCTDAKA"]
+    COOKIE_BUCTDAKA = os.environ["COOKIE_BUCTDAKA"]
+    CREATED_BUCTDAKA = os.environ["CREATED_BUCTDAKA"]
+    DATAS_BUCTDAKA = os.environ["DATAS_BUCTDAKA"]
+    GEOAPIINFO_BUCTDAKA = os.environ["GEOAPIINFO_BUCTDAKA"]
+    HEADERS_BUCTDAKA = os.environ["HEADERS_BUCTDAKA"]
+    ID_BUCTDAKA = os.environ["ID_BUCTDAKA"]
+    PROVINCE_BUCTDAKA = os.environ["PROVINCE_BUCTDAKA"]
+    UID_BUCTDAKA = os.environ["UID_BUCTDAKA"]
+    USERAGENT_BUCTDAKA = os.environ["USERAGENT_BUCTDAKA"]
+    
+    headers = {
     'Host': 'eai.buct.edu.cn',
     'Connection': 'keep-alive',
     'Content-Length': '2014',
@@ -21,81 +36,67 @@ headers = {
     
     }
 
-datas = {
-    'ismoved': '0',
-    'jhfjrq' : '',
-    'jhfjjtgj': '',
-    'jhfjhbcc': '',
-    'sfxk': '0',
-    'xkqq': '',
-    'szgj': '',
-    'szcs': '',
-    'zgfxdq': '0',
-    'mjry': '0',
-    'csmjry': '0',
-    'tw': '2',
-    'sfcxtz': '0',
-    'sfjcbh': '0',
-    'sfcxzysx': '0',
-    'sfyyjc': '0',
-    'jcjgqr': '0',
-    'remark': 'Good赞666666',
-    'address': ADDRESS_BUCTDAKA,
-    'geo_api_info': GEOAPIINFO_BUCTDAKA,
-    'area': AREA_BUCTDAKA,
-    'province': PROVINCE_BUCTDAKA,
-    'city': CITY_BUCTDAKA,
-    'sfzx' : '0',
-    'sfjcwhry': '0',
-    'sfjchbry': '0',
-    'sfcyglq': '0',
-    'gllx': '',
-    'glksrq': '',
-    'jcbhlx': '',
-    'jcbhrq': '',
-    'bztcyy': '',
-    'sftjhb': '0',
-    'sftjwh': '0',
-    'sfsfbh': '0',
-    'xjzd': '',
-    'jcwhryfs': '',
-    'jchbryfs': '',
-    'szsqsfybl': '0',
-    'sfygtjzzfj': '',
-    'gtjzzfjsj': '',
-    'jcjg': '',
-    'date': getdate(),
-    'uid': UID_BUCTDAKA,
-    'created': CREATED_BUCTDAKA,
-    'id': ID_BUCTDAKA,
-    'gwszdd': '',
-    'sfyqjzgc': '',
-    'jcqzrq': '',
-    'sfjcqz': '',
-    'jrsfqzys': '',
-    'jrsfqzfy': '',
-    'sfsqhzjkk': '',
-    'sqhzjkkys': '',
-    'qksm': '',
-        
-    }
+    datas = {
+        'ismoved': '0',
+        'jhfjrq' : '',
+        'jhfjjtgj': '',
+        'jhfjhbcc': '',
+        'sfxk': '0',
+        'xkqq': '',
+        'szgj': '',
+        'szcs': '',
+        'zgfxdq': '0',
+        'mjry': '0',
+        'csmjry': '0',
+        'tw': '2',
+        'sfcxtz': '0',
+        'sfjcbh': '0',
+        'sfcxzysx': '0',
+        'sfyyjc': '0',
+        'jcjgqr': '0',
+        'remark': 'Good赞666666',
+        'address': ADDRESS_BUCTDAKA,
+        'geo_api_info': GEOAPIINFO_BUCTDAKA,
+        'area': AREA_BUCTDAKA,
+        'province': PROVINCE_BUCTDAKA,
+        'city': CITY_BUCTDAKA,
+        'sfzx' : '0',
+        'sfjcwhry': '0',
+        'sfjchbry': '0',
+        'sfcyglq': '0',
+        'gllx': '',
+        'glksrq': '',
+        'jcbhlx': '',
+        'jcbhrq': '',
+        'bztcyy': '',
+        'sftjhb': '0',
+        'sftjwh': '0',
+        'sfsfbh': '0',
+        'xjzd': '',
+        'jcwhryfs': '',
+        'jchbryfs': '',
+        'szsqsfybl': '0',
+        'sfygtjzzfj': '',
+        'gtjzzfjsj': '',
+        'jcjg': '',
+        'date': getdate(),
+        'uid': UID_BUCTDAKA,
+        'created': CREATED_BUCTDAKA,
+        'id': ID_BUCTDAKA,
+        'gwszdd': '',
+        'sfyqjzgc': '',
+        'jcqzrq': '',
+        'sfjcqz': '',
+        'jrsfqzys': '',
+        'jrsfqzfy': '',
+        'sfsqhzjkk': '',
+        'sqhzjkkys': '',
+        'qksm': '',
+            
+        }
 
-url = 'https://eai.buct.edu.cn/ncov/wap/default/save'
-
-
-if __name__ == '__main__':
-    ADDRESS_BUCTDAKA = os.environ["ADDRESS_BUCTDAKA"]
-    AREA_BUCTDAKA = os.environ["AREA_BUCTDAKA"]
-    CITY_BUCTDAKA = os.environ["CITY_BUCTDAKA"]
-    COOKIE_BUCTDAKA = os.environ["COOKIE_BUCTDAKA"]
-    CREATED_BUCTDAKA = os.environ["CREATED_BUCTDAKA"]
-    DATAS_BUCTDAKA = os.environ["DATAS_BUCTDAKA"]
-    GEOAPIINFO_BUCTDAKA = os.environ["GEOAPIINFO_BUCTDAKA"]
-    HEADERS_BUCTDAKA = os.environ["HEADERS_BUCTDAKA"]
-    ID_BUCTDAKA = os.environ["ID_BUCTDAKA"]
-    PROVINCE_BUCTDAKA = os.environ["PROVINCE_BUCTDAKA"]
-    UID_BUCTDAKA = os.environ["UID_BUCTDAKA"]
-    USERAGENT_BUCTDAKA = os.environ["USERAGENT_BUCTDAKA"]
+    url = 'https://eai.buct.edu.cn/ncov/wap/default/save'
+    
     result = requests.post(url=url, headers=headers, data = datas)
     print(result.text)
     # print(getdate())
