46cc7b3c1204f755dbd872649fb1c22b5ca998cb
LetMeFly666 <814114971@qq.com>
2021-01-29 15:42:05 +0800

    Random to：-40073758

--- 自动脚本/TFpacket/RandomPush.py
+++ 自动脚本/TFpacket/RandomPush.py
@@ -7,7 +7,7 @@ with open("Old\Random.txt","w")as f:
     f.write(str(randNum))
 print(randNum)
 os.system("git add .")
-os.system('git commit -m "Random to'+str(randNum)+'"')
+os.system('git commit -m "Random to：'+str(randNum)+'"')
 os.system("git push -u original master")
 print('\a')
 time.sleep(4)
