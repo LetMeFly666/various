4b60f052dc0f810cde5e3f1ade5d1c9f43f85d11
LetMeFly666 <814114971@qq.com>
2021-01-30 23:00:15 +0800

    Add the poem: 时间并不存在，存在的只有此刻。

--- 自动脚本/TFpacket/RandPoemAndPush.py
+++ 自动脚本/TFpacket/RandPoemAndPush.py
@@ -11,11 +11,17 @@ def main(need = 1, maxTried = 50):
         if i != 0:
             toCommit += '、'
         toCommit += ' ' + get[i]
+    toCommit2 = toCommit.split('"')
+    toCommit3 = ''
+    for i in toCommit2:
+        toCommit3 += i
     os.system("git add .")
-    realCommit = f'git commit -m "{toCommit}"'
+    realCommit = f'git commit -m "{toCommit3}"'
     print(realCommit)
-    os.system(realCommit)    
+    os.system(realCommit)
     os.system("git push -u original master")
+    print('\a')
+    time.sleep(4)
 
 if __name__ == "__main__":
     main()
