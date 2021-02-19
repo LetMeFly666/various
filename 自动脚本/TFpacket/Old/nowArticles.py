9783dd5e59f71019edcabc4a1153169b29d1363d
LetMeFly666 <814114971@qq.com>
2021-02-19 23:18:25 +0800

    Add the poem: 为错误停驻是件可悲的事。

--- 自动脚本/TFpacket/Old/nowArticles.py
+++ 自动脚本/TFpacket/Old/nowArticles.py
@@ -10,4 +10,4 @@ nowArticles = [
         'https://blog.csdn.net/Tisfy/article/details/104761388',
         'https://blog.csdn.net/Tisfy/article/details/104717034',
     ]
-# 昨天中午更改的不知道什么时候同步上了，这次更改一下其他目录下的试试。（2021.2.19-19:13）
+# 看来在git的目录下重新保存一下就可了。这次在另外目录下保存，过一段时间再看有没有同步上去。
