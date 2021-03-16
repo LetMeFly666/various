19ec1d61ab10c3b624de56332cecec9b63fb7924
LetMeFly666 <814114971@qq.com>
2021-03-16 17:47:24 +0800

    变成了原有的日期

--- ACM/ForBUCToj/url.js
+++ ACM/ForBUCToj/url.js
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2021-03-16 10:51:51
  * @LastEditors: LetMeFly
- * @LastEditTime: 2021-03-16 17:42:18
+ * @LastEditTime: 2021-03-16 17:46:46
  */
 
 //#region function
@@ -24,7 +24,7 @@ function jump() {
 const couldPic = 3
 const couldnotPic = 6
 const couldSeeList = {
-    5173: { "time": new Date("2021-03-15 16:25:0"), "explanationHref": "https://blog.csdn.net/Tisfy/article/details/114788865", "problemHref": "http://39.106.31.26/problem.php?id=5173" },
+    5173: { "time": new Date("2021-03-25 16:25:0"), "explanationHref": "https://blog.csdn.net/Tisfy/article/details/114788865", "problemHref": "http://39.106.31.26/problem.php?id=5173" },
 }
 //#endregion
 
