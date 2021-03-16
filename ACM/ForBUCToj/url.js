003b0ef3e1a20c296c02217eb7d0754031d16dd7
LetMeFly666 <814114971@qq.com>
2021-03-16 15:32:02 +0800

    回归了真正的模样

--- ACM/ForBUCToj/url.js
+++ ACM/ForBUCToj/url.js
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2021-03-16 10:51:51
  * @LastEditors: LetMeFly
- * @LastEditTime: 2021-03-16 15:20:40
+ * @LastEditTime: 2021-03-16 15:31:35
  */
 
 //#region function
@@ -23,8 +23,7 @@ function jump() {
 const couldPic = 3
 const couldnotPic = 6
 const couldSeeList = {
-    //5173: { "time": new Date("2021-03-25 16:25:0"), "explanationHref": "https://blog.csdn.net/Tisfy/article/details/114788865", "problemHref": "http://39.106.31.26/problem.php?id=5173" },
-    5173: { "time": new Date("2021-03-16 15:30:0"), "explanationHref": "https://blog.csdn.net/Tisfy/article/details/114788865", "problemHref": "http://39.106.31.26/problem.php?id=5173" },
+    5173: { "time": new Date("2021-03-25 16:25:0"), "explanationHref": "https://blog.csdn.net/Tisfy/article/details/114788865", "problemHref": "http://39.106.31.26/problem.php?id=5173" },
 }
 //#endregion
 
