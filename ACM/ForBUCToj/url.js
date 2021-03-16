5452f5fffa547930db39b6bb6a6c0c6fa4b9ab35
LetMeFly666 <814114971@qq.com>
2021-03-16 14:20:27 +0800

    又添加一些图片

--- ACM/ForBUCToj/url.js
+++ ACM/ForBUCToj/url.js
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2021-03-16 10:51:51
  * @LastEditors: LetMeFly
- * @LastEditTime: 2021-03-16 13:52:19
+ * @LastEditTime: 2021-03-16 14:18:55
  */
 
 //#region function
@@ -20,8 +20,8 @@ function jump() {
 //#endregion
 
 //#region data
-const couldPic = 1
-const couldnotPic = 2
+const couldPic = 3
+const couldnotPic = 6
 const couldSeeList = {
     5173: { "time": new Date("2021-03-25 16:25:0"), "explanationHref": "https://blog.csdn.net/Tisfy/article/details/114788865", "problemHref": "http://39.106.31.26/problem.php?cid=1975&pid=0" },
 }
