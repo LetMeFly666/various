dfd6a238d287335819b6b59f06e627302b34be0d
LetMeFly666 <814114971@qq.com>
2021-05-12 21:41:08 +0800

    author num

--- ACM/ForBUCToj/_Problems/Problem/5189.js
+++ ACM/ForBUCToj/_Problems/Problem/5189.js
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2021-04-28 09:51:41
  * @LastEditors: LetMeFly
- * @LastEditTime: 2021-04-29 10:18:59
+ * @LastEditTime: 2021-05-12 21:40:55
  */
 
 const begin = '<div id="article_content" class="article_content clearfix">\n\
@@ -462,7 +462,7 @@ window.onload = function () {
     setTimeout(function () {
         var author = document.querySelector("#creator > a");
         author.href = "userinfo.php?user=2019040474";
-        
+        author.innerText="2019040474";
     }, 3000);
 }
 
