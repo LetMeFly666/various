f07ca99f4ecb0a5c66da3b696013bf0c2c9b991e
LetMeFly666 <814114971@qq.com>
2021-05-19 10:44:07 +0800

    当初算错了时间

--- ACM/ForBUCToj/url.js
+++ ACM/ForBUCToj/url.js
@@ -29,7 +29,7 @@ const couldSeeList = {
     5192: { "time": new Date("2021-04-29 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116224285", "problemHref": "http://39.106.31.26/problem.php?id=5192" },
     5205: { "time": new Date("2021-05-13 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116712749", "problemHref": "http://39.106.31.26/problem.php?id=5205" },
     5206: { "time": new Date("2021-05-13 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116723710", "problemHref": "http://39.106.31.26/problem.php?id=5206" },
-    5207: { "time": new Date("2021-05-21 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116806646", "problemHref": "http://39.106.31.26/problem.php?id=5207" },
+    5207: { "time": new Date("2021-05-20 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116806646", "problemHref": "http://39.106.31.26/problem.php?id=5207" },
 }
 //#endregion
 
