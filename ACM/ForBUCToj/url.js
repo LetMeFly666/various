c7d2b913b39de9eac6b4d1597a837289b42c5081
LetMeFly666 <814114971@qq.com>
2021-06-03 11:45:20 +0800

    add 5220

--- ACM/ForBUCToj/url.js
+++ ACM/ForBUCToj/url.js
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2021-03-16 10:51:51
  * @LastEditors: LetMeFly
- * @LastEditTime: 2021-05-27 23:52:48
+ * @LastEditTime: 2021-06-03 11:44:49
  */
 
 //#region function
@@ -10,6 +10,7 @@ dbg = toDebug => console.log((typeof toDebug) + "\n" + toDebug)
 ifCouldSee = problemId => new Date().getTime() > couldSeeList[problemId]["time"].getTime()
 couldPicUrl = () => `url('./Could/${Math.floor(Math.random() * 1000000) % couldPic + 1}.jpg')`
 couldnotPicUrl = () => `url('./Couldnot/${Math.floor(Math.random() * 1000000) % couldnotPic + 1}.jpg')`
+
 function jump() {
     outTime -= 1
     if (outTime == 0) {
@@ -24,16 +25,18 @@ function jump() {
 const couldPic = 3
 const couldnotPic = 6
 const couldSeeList = {
-    5173: { "time": new Date("2021-03-25 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/114788865", "problemHref": "http://39.106.31.26/problem.php?id=5173" },
-    5189: { "time": new Date("2021-04-29 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116211565", "problemHref": "http://39.106.31.26/problem.php?id=5189" },
-    5192: { "time": new Date("2021-04-29 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116224285", "problemHref": "http://39.106.31.26/problem.php?id=5192" },
-    5205: { "time": new Date("2021-05-13 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116712749", "problemHref": "http://39.106.31.26/problem.php?id=5205" },
-    5206: { "time": new Date("2021-05-13 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116723710", "problemHref": "http://39.106.31.26/problem.php?id=5206" },
-    5207: { "time": new Date("2021-05-20 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116806646", "problemHref": "http://39.106.31.26/problem.php?id=5207" },
-    5209: { "time": new Date("2021-05-20 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/117063879", "problemHref": "http://39.106.31.26/problem.php?id=5209" },
-    5217: { "time": new Date("2021-05-30 12:00:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/117339731", "problemHref": "http://39.106.31.26/problem.php?id=5217" },
-}
-//#endregion
+        5173: { "time": new Date("2021-03-25 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/114788865", "problemHref": "http://39.106.31.26/problem.php?id=5173" },
+        5189: { "time": new Date("2021-04-29 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116211565", "problemHref": "http://39.106.31.26/problem.php?id=5189" },
+        5192: { "time": new Date("2021-04-29 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116224285", "problemHref": "http://39.106.31.26/problem.php?id=5192" },
+        5205: { "time": new Date("2021-05-13 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116712749", "problemHref": "http://39.106.31.26/problem.php?id=5205" },
+        5206: { "time": new Date("2021-05-13 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116723710", "problemHref": "http://39.106.31.26/problem.php?id=5206" },
+        5207: { "time": new Date("2021-05-20 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/116806646", "problemHref": "http://39.106.31.26/problem.php?id=5207" },
+        5209: { "time": new Date("2021-05-20 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/117063879", "problemHref": "http://39.106.31.26/problem.php?id=5209" },
+        5217: { "time": new Date("2021-05-30 12:00:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/117339731", "problemHref": "http://39.106.31.26/problem.php?id=5217" },
+        5219: { "time": new Date("2021-05-30 12:00:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/117353291", "problemHref": "http://39.106.31.26/problem.php?id=5219" },
+        5220: { "time": new Date("2021-06-03 16:25:0"), "explanationHref": "https://letmefly.blog.csdn.net/article/details/117509460", "problemHref": "http://39.106.31.26/problem.php?id=5220" },
+    }
+    //#endregion
 
 //#region main
 const problemId = location.href.split('?')[1].split('=')[1]
@@ -44,4 +47,4 @@ document.body.style.backgroundPosition = "center"
 document.body.style.backgroundRepeat = "no-repeat"
 document.body.style.backgroundAttachment = "fixed";
 jump()
-//#endregion
+    //#endregion
