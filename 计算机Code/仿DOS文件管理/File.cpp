6bba60023566ee18951c6671d80a878c4a153ad5
LetMeFly666 <814114971@qq.com>
2021-07-08 00:10:31 +0800

    中途push

--- 计算机Code/仿DOS文件管理/File.cpp
+++ 计算机Code/仿DOS文件管理/File.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2021-07-07 17:05:34
  * @LastEditors: LetMeFly
- * @LastEditTime: 2021-07-08 00:06:47
+ * @LastEditTime: 2021-07-08 00:10:22
  */
 #include <bits/stdc++.h>
 #include <windows.h>
@@ -404,6 +404,32 @@ void execute() // 执行
                     else
                     {
                         cout << "`" << toReturn[i] << "`已存在" << endl;
+                    }
+                }
+            }
+        }
+        else if (toReturn[0] == "show")
+        {
+            if (toReturn.size() != 2)
+            {
+                puts("参数错误");
+            }
+            else
+            {
+                if (!alreadyExists(pfcb->childs, toReturn[1]))
+                {
+                    puts("系统找不到指定文件");
+                }
+                else
+                {
+                    PFcb pFcb = findChildByName(pfcb, toReturn[1]);
+                    if (pFcb -> isFile)
+                    {
+                        cout<<"文件的内容为："<<pFcb->content<<endl;
+                    }
+                    else
+                    {
+                        puts("不是文件");
                     }
                 }
             }
