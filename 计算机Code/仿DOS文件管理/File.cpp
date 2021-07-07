d616e9d2d647a4cb22ca2f59cb40d581d193de99
LetMeFly666 <814114971@qq.com>
2021-07-07 18:36:01 +0800

    中途push

--- 计算机Code/仿DOS文件管理/File.cpp
+++ 计算机Code/仿DOS文件管理/File.cpp
@@ -2,13 +2,14 @@
  * @Author: LetMeFly
  * @Date: 2021-07-07 17:05:34
  * @LastEditors: LetMeFly
- * @LastEditTime: 2021-07-07 18:34:17
+ * @LastEditTime: 2021-07-07 18:36:00
  */
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef vector<string> ToReturn;  // 专门用来存放返回数据的一种数据类型
 typedef vector<string> Path;  // 路径
+Path path;  // 真正的路径
 
 ToReturn split(string toSplit, char c)  // 将字符串以字符c为间隔分开
 {
@@ -39,6 +40,7 @@ void help()
 
 void init()
 {
+    path.push_back("root");
     copyright();
 }
 
