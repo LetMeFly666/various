4073b4374ffc187fb4b8b3af31c48301f1014678
LetMeFly666 <814114971@qq.com>
2021-07-09 14:04:20 +0800

    change

--- 计算机Code/排五七星加数器/Calculator.py
+++ 计算机Code/排五七星加数器/Calculator.py
@@ -60,8 +60,10 @@ frame_operate.place(x=frame_operate_x, y=frame_operate_y)
 # 头
 frame_operate_head = tk.Frame(frame_operate, bg="blue", height=frame_operate_height/4, width=frame_operate_width)
 frame_operate_head.place(x=0, y=0)
-frame_operate_head_text = tk.Label(frame_operate_head, text="头", font=('Arial', 14), fg="black", width=5, height=3, bg="red")
+frame_operate_head_text = tk.Label(frame_operate_head, text="头：", font=('Arial', 14), fg="black", width=7, height=3, bg="red")
 frame_operate_head_text.place(x=0, y=0)
+frame_operate_head_input = tk.Entry(frame_operate_head, bg="gold", font=('Arial', 14), width=9)
+frame_operate_head_input.place(x=55, y=23)
 # 尾
 frame_operate_tail = tk.Frame(frame_operate, bg="yellow", height=frame_operate_height/4, width=frame_operate_width)
 frame_operate_tail.place(x=0, y=frame_operate_height/4)
