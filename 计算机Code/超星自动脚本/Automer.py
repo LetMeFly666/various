1fd6292536086e205efa92e2d9368cb9e7da2b94
LetMeFly666 <814114971@qq.com>
2021-07-09 23:36:36 +0800

    add 07

---
+++ 计算机Code/超星自动脚本/Automer.py
@@ -0,0 +1,21 @@
+
+import CheckNetwork
+
+__exeName__ = "007_auto_chaoxing"
+__exeVersion__ = 1
+
+"""
+;下面的注释包含自解压脚本命令
+
+Setup=Automer.exe
+Text
+{
+绿色安装，附加任务：创建桌面快捷方式。此外无其他盘符占用 
+}
+Shortcut=D, Automer.exe, , 双击运行, 超星自动播放, Img/icon.ico
+
+"""
+
+
+CheckNetwork.main(__exeName__, __exeVersion__)
+
