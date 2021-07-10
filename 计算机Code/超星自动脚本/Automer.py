a804790b60756bbf3325e5c98cd0a15b9e25113a
LetMeFly666 <814114971@qq.com>
2021-07-10 17:22:02 +0800

    change

--- 计算机Code/超星自动脚本/Automer.py
+++ 计算机Code/超星自动脚本/Automer.py
@@ -1,7 +1,12 @@
+from threading import Thread
 import CheckNetwork
+import Automer_tk
 
 __exeName__ = "007_auto_chaoxing"
 __exeVersion__ = 1
 
+CheckNetwork.main(__exeName__, __exeVersion__)  # FIXME: 取消注释
+
+Automer_tk.main()
 
 
--- 计算机Code/超星自动脚本/Automer_tk.py
+++ 计算机Code/超星自动脚本/Automer_tk.py
@@ -1,2 +1,16 @@
+import Automer_WebDriver
 import tkinter as tk
 
+
+def main():
+    window = tk.Tk()
+    window.title("超星播放器")
+    try:
+        window.title('Img/icon.ico')
+    except:
+        pass
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
