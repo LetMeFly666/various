25a4c488ed94a323fe1f869689a5b38330fd8f77
LetMeFly666 <814114971@qq.com>
2021-07-15 19:57:16 +0800

    更新了题面

--- ACM/ForBUCToj/_Problems/Problem/5250.js
+++ ACM/ForBUCToj/_Problems/Problem/5250.js
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2021-07-15 19:51:43
  * @LastEditors: LetMeFly
- * @LastEditTime: 2021-07-15 19:52:30
+ * @LastEditTime: 2021-07-15 19:57:01
  */
 /*
  * @Author: LetMeFly
@@ -27,21 +27,42 @@ const end = '                </div><div data-report-view="{&quot;mod&quot;:&quot
 
 
 const problem_description_data = '\n\
-<p>自从20世纪90年代初Python语言诞生至今&#xff0c;它已被逐渐广泛应用于系统管理任务的处理和<a href="https://baike.baidu.com/item/Web/150564">Web</a>编程。</p> \n\
-<p>Python的创始人为<a href="https://baike.baidu.com/item/%E5%90%89%E5%A4%9A%C2%B7%E8%8C%83%E7%BD%97%E8%8B%8F%E5%A7%86/328361">荷兰人吉多·范罗苏姆</a> &#xff08;Guido van Rossum&#xff09;。1989年圣诞节期间&#xff0c;在<a href="https://baike.baidu.com/item/%E9%98%BF%E5%A7%86%E6%96%AF%E7%89%B9%E4%B8%B9/2259975">阿姆斯特丹</a>&#xff0c;Guido为了打发<a href="https://baike.baidu.com/item/%E5%9C%A3%E8%AF%9E%E8%8A%82/127881">圣诞节</a>的无趣&#xff0c;决心开发一个新的脚本解释程序&#xff0c;作为<a href="https://baike.baidu.com/item/ABC%E8%AF%AD%E8%A8%80/334996">ABC语言</a>的一种继承。之所以选中Python&#xff08;大蟒蛇的意思&#xff09;作为该编程语言的名字&#xff0c;是取自英国20世纪70年代首播的电视喜剧《蒙提·派森的飞行马戏团》&#xff08;Monty Python’s Flying Circus&#xff09;。</p>\n<p>Python是完全面向对象的语言。函数、模块、数字、字符串都是对象。并且完全支持继承、重载、派生、多继承&#xff0c;有益于增强源代码的复用性。Python支持重载运算符和动态类型。相对于Lisp这种传统的函数式编程语言&#xff0c;Python对函数式设计只提供了有限的支持。有两个标准库(functools, itertools)提供了Haskell和Standard ML中久经考验的函数式程序设计工具。</p>\n<p>有些比赛&#xff0c;不限制语言种类&#xff0c;遇到一些对时间要求不高的&#xff0c;用C缺较复杂的题目&#xff0c;不妨试试用python来做。</p>\n\
-<p>比如要计算大整数加法&#xff0c;用Python就很容易&#xff08;此处没有语言偏见&#xff09;。</p> \n\
-<p>Python 已经成为最受欢迎的程序设计语言之一。自从2004年以后&#xff0c;python的使用率呈线性增长。Python 2于2000年10月16日发布&#xff0c;稳定版本是Python 2.7。Python 3于2008年12月3日发布&#xff0c;不完全兼容Python 2。 2011年1月&#xff0c;它被TIOBE编程语言排行榜评为2010年度语言。</p> \n\
-<p>现在给你两个换行相隔的正整数&#xff0c;范围是1~10的100000次方&#xff0c;保证没有前导零&#xff0c;请你计算这两个数的和&#xff0c;同时也请不要输出前导0。</p> \n\
-<p>Python本身被设计为可扩充的。并非所有的特性和功能都集成到语言核心。Python提供了丰富的API和工具&#xff0c;以便程序员能够轻松地使用C语言、C&#43;&#43;、Cython来编写扩充模块。</p> \n\
-<p>由于Python语言的简洁性、易读性以及可扩展性&#xff0c;在国外用Python做科学计算的研究机构日益增多&#xff0c;一些知名大学已经采用Python来教授程序设计课程。</p>\n\
-<p>非输入输出相关部分来自<a href="https://baike.baidu.com/">百度百科</a></p> \n\
+<p>宋代史学家司马光在《资治通鉴》中有一段著名的“德才论”&#xff1a;“是故才德全尽谓之圣人&#xff0c;才德兼亡谓之愚人&#xff0c;德胜才谓之君子&#xff0c;才胜德谓之小人。凡取人之术&#xff0c;苟不得圣人&#xff0c;君子而与之&#xff0c;与其得小人&#xff0c;不若得愚人。”</p> \n\
+<p>现给出一批考生的德才分数&#xff0c;请根据司马光的理论给出录取排名。</p>  \n\
 <hr /> \n\
 '
 
 
-const input_description_data = '<p>这是一道阅读理解题&#xff0c;输入描述就在题目中。</p> '
+const input_description_data = '\n\
+<p>输入第一行给出 3 个正整数&#xff0c;分别为&#xff1a;N&#xff08;<span class="katex--inline"><span class="katex"><span class="katex-mathml">\n\
+    \n\
+     \n\
+      \n\
+       \n\
+        ≤\n\
+       \n\
+       \n\
+        1\n\
+       \n\
+       \n\
+        \n\
+         0\n\
+        \n\
+        \n\
+         5\n\
+        \n\
+       \n\
+      \n\
+      \n\
+       ≤10^{5}\n\
+      \n\
+     \n\
+    </span><span class="katex-html"><span class="base"><span class="strut" style="height: 0.77194em; vertical-align: -0.13597em;"></span><span class="mrel">≤</span><span class="mspace" style="margin-right: 0.277778em;"></span></span><span class="base"><span class="strut" style="height: 0.814108em; vertical-align: 0em;"></span><span class="mord">1</span><span class="mord"><span class="mord">0</span><span class="msupsub"><span class="vlist-t"><span class="vlist-r"><span class="vlist" style="height: 0.814108em;"><span class="" style="top: -3.063em; margin-right: 0.05em;"><span class="pstrut" style="height: 2.7em;"></span><span class="sizing reset-size6 size3 mtight"><span class="mord mtight"><span class="mord mtight">5</span></span></span></span></span></span></span></span></span></span></span></span></span>&#xff09;&#xff0c;即考生总数&#xff1b;L&#xff08;≥60&#xff09;&#xff0c;为录取最低分数线&#xff0c;即德分和才分均不低于 L 的考生才有资格被考虑录取&#xff1b;H&#xff08;&lt;100&#xff09;&#xff0c;为优先录取线——德分和才分均不低于此线的被定义为“才德全尽”&#xff0c;此类考生按德才总分从高到低排序&#xff1b;才分不到但德分到线的一类考生属于“德胜才”&#xff0c;也按总分排序&#xff0c;但排在第一类考生之后&#xff1b;德才分均低于 H&#xff0c;但是德分不低于才分的考生属于“才德兼亡”但尚有“德胜才”者&#xff0c;按总分排序&#xff0c;但排在第二类考生之后&#xff1b;其他达到最低线 L 的考生也按总分排序&#xff0c;但排在第三类考生之后。</p> \n\
+<p>随后 N 行&#xff0c;每行给出一位考生的信息&#xff0c;包括&#xff1a;准考证号、德分、才分&#xff0c;其中准考证号为 8 位整数&#xff0c;德才分为区间 [0, 100] 内的整数。数字间以空格分隔。</p> \n\
+<hr /> \n'
 
-const output_description_data = '<p>当然也在题目中。<br /> 哦&#xff0c;对了&#xff0c;不在题目描述中的只有一点&#xff1a;<strong>这次比赛不能用Python</strong>。</p> '
+const output_description_data = '<p>输出第一行首先给出达到最低分数线的考生人数 M&#xff0c;随后 M 行&#xff0c;每行按照输入格式输出一位考生的信息&#xff0c;考生按输入中说明的规则从高到低排序。当某类考生中有多人总分相同时&#xff0c;按其德分降序排列&#xff1b;若德分也并列&#xff0c;则按准考证号的升序输出。</p> \n\
+<hr /> \n'
 
 window.onload = function () {
     var isProblem = 1;
