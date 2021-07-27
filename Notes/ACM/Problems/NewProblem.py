1ea6dbe88038a8fbf7cd55f402d057e274dd02dc
LetMeFly666 <814114971@qq.com>
2021-07-27 15:46:14 +0800

    tryAgain

--- Notes/ACM/Problems/NewProblem.py
+++ Notes/ACM/Problems/NewProblem.py
@@ -26,20 +26,21 @@ index_qian = '''<iframe id="frame1" src="Description" frameborder="0" scrolling=
 
 import os
 basePath='f:\\OtherApps\\Program\\Git\\Store\\Store2_Web_Various\\various\\Notes\\ACM\\Problems\\'
-name = basePath+input("新问题的名称[64位整数乘法]：")
+name = input("新问题的名称[64位整数乘法]：")
+fullName = basePath+name
 online_name = input("在线题目名称[AcWing90]：")
 online_address = input("题目地址[https://www.acwing.com/problem/content/92/]：")
 
-if not os.path.exists(name):
-    os.mkdir(name)
-    os.mkdir(f'{name}\\Description')
-    with open(f"{name}\\Description\\Readme.md", 'w', encoding='utf-8')as f:
-        f.write(f'# {name}')
-    with open(f"{name}\\index.html", 'w', encoding='utf-8')as f:
+if not os.path.exists(fullName):
+    os.mkdir(fullName)
+    os.mkdir(f'{fullName}\\Description')
+    with open(f"{fullName}\\Description\\Readme.md", 'w', encoding='utf-8')as f:
+        f.write(f'# {fullName}')
+    with open(f"{fullName}\\index.html", 'w', encoding='utf-8')as f:
         f.write(index_qian)
-    with open(f"{name}\\main.cpp", 'w', encoding='utf-8')as f:
+    with open(f"{fullName}\\main.cpp", 'w', encoding='utf-8')as f:
         f.write('')
-    with open("index.html", "a+", encoding='utf-8')as f:
-        f.write(f'<a href="{name}/">{name}</a> <small><a href="{online_address}">{online_name}</a></small><br /><hr />')
+    with open(f"{basePath}index.html", "a+", encoding='utf-8')as f:
+        f.write(f'\n<a href="{name}/">{name}</a> <small><a href="{online_address}">{online_name}</a></small><br /><hr />\n')
 else:
     print("此文件夹已存在")
