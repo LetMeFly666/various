696d49291ddddedeeac6d0d9cdd5de7184644d6b
LetMeFly666 <814114971@qq.com>
2021-12-11 14:40:18 +0800

    try again

--- 自动脚本/BUCTdaka.py
+++ 自动脚本/BUCTdaka.py
@@ -23,8 +23,9 @@ if __name__ == '__main__':
     AREA_BUCTDAKA = os.environ["AREA_BUCTDAKA"]
     CITY_BUCTDAKA = os.environ["CITY_BUCTDAKA"]
     COOKIE_BUCTDAKA = os.environ["COOKIE_BUCTDAKA"]
-    # COOKIE_BUCTDAKA_MAOMAO = os.environ["COOKIE_BUCTDAKA_MAOMAO"]
-    COOKIE_BUCTDAKA_MAOMAO = '5'
+    COOKIE_BUCTDAKA_MAOMAO = os.environ["COOKIE_BUCTDAKA_MAOMAO"]
+    # COOKIE_BUCTDAKA_MAOMAO = '5'
+    print(os.environ)
     CREATED_BUCTDAKA = os.environ["CREATED_BUCTDAKA"]
     DATAS_BUCTDAKA = os.environ["DATAS_BUCTDAKA"]
     GEOAPIINFO_BUCTDAKA = os.environ["GEOAPIINFO_BUCTDAKA"]
