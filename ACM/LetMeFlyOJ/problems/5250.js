7b0758ab2e8fba3e6cb98118b007e81a97e2c3db
LetMeFly666 <814114971@qq.com>
2023-04-24 15:56:53 +0800

    修改了LetMeFlyOJ的title，将标签及id添加了上去

--- ACM/LetMeFlyOJ/problems/5250.js
+++ ACM/LetMeFlyOJ/problems/5250.js
@@ -2,9 +2,9 @@
  * @Author: LetMeFly
  * @Date: 2022-05-05 09:17:33
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-05-05 09:17:33
+ * @LastEditTime: 2023-04-24 15:56:09
  */
-const LetMeFlyOJ_title = "调皮的摩尔";
+const LetMeFlyOJ_title = "<t id='LetMeFly_colorFul0'>调皮的摩尔</t>";
 const LetMeFlyOJ_inputData = "LetMeFly";
 const LetMeFlyOJ_outputData = "89816878890";
 const LetMeFlyOJ_memory = "128M";
