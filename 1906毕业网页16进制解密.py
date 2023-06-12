e2fe28ec99602af4cc64a883a7a277f19972f8f4
LetMeFly666 <814114971@qq.com>
2023-06-12 17:15:42 +0800

    ok

---
+++ 1906毕业网页16进制解密.py
@@ -0,0 +1,9 @@
+'''
+Author: LetMeFly
+Date: 2023-06-12 12:45:40
+LastEditors: LetMeFly
+LastEditTime: 2023-06-12 17:13:46
+'''
+import binascii
+with open('answer.txt', 'wb') as f:
+    f.write(binascii.unhexlify('3139303642455354'))
