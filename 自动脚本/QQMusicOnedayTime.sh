ed924194a5b59b0b247dc18df7bc524729f75194
LetMeFly666 <814114971@qq.com>
2023-08-24 23:15:06 +0800

    forgetted to rederect to nul and try 200 times once

--- 自动脚本/QQMusicOnedayTime.sh
+++ 自动脚本/QQMusicOnedayTime.sh
@@ -2,12 +2,12 @@
  # @Author: LetMeFly
  # @Date: 2023-08-24 23:01:18
  # @LastEditors: LetMeFly
- # @LastEditTime: 2023-08-24 23:13:26
+ # @LastEditTime: 2023-08-24 23:14:42
 ### 
 #!/bin/bash
 
-for (( i=1; i<=3; i++ ))
+for (( i=1; i<=200; i++ ))
 do
     echo $i
-    curl -X POST -d "$QQMUSICONEDAYTIME_DATA" "https://stat.y.qq.com/pc/fcgi-bin/reportmus.fcg?pcachetime=1692368054"
+    curl -X POST -d "$QQMUSICONEDAYTIME_DATA" "https://stat.y.qq.com/pc/fcgi-bin/reportmus.fcg?pcachetime=1692368054" > nul
 done
