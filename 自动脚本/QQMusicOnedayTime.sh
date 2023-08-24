72ea7c5a76aded8a0a55ffdbb9004d704f81c1b6
LetMeFly666 <814114971@qq.com>
2023-08-24 23:13:53 +0800

    curl

--- 自动脚本/QQMusicOnedayTime.sh
+++ 自动脚本/QQMusicOnedayTime.sh
@@ -2,12 +2,12 @@
  # @Author: LetMeFly
  # @Date: 2023-08-24 23:01:18
  # @LastEditors: LetMeFly
- # @LastEditTime: 2023-08-24 23:12:11
+ # @LastEditTime: 2023-08-24 23:13:26
 ### 
 #!/bin/bash
 
 for (( i=1; i<=3; i++ ))
 do
     echo $i
-    curl -X POST -d "$QQMUSICONEDAYTIME_DATA"
+    curl -X POST -d "$QQMUSICONEDAYTIME_DATA" "https://stat.y.qq.com/pc/fcgi-bin/reportmus.fcg?pcachetime=1692368054"
 done
