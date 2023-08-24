86600315f4135d66b9cca86d2a5b623e3d30b493
LetMeFly666 <814114971@qq.com>
2023-08-24 23:12:26 +0800

    hide data

--- 自动脚本/QQMusicOnedayTime.sh
+++ 自动脚本/QQMusicOnedayTime.sh
@@ -2,16 +2,12 @@
  # @Author: LetMeFly
  # @Date: 2023-08-24 23:01:18
  # @LastEditors: LetMeFly
- # @LastEditTime: 2023-08-24 23:01:19
+ # @LastEditTime: 2023-08-24 23:12:11
 ### 
 #!/bin/bash
 
 for (( i=1; i<=3; i++ ))
 do
-    curl "https://www.baidu.com"
-done
-
-for (( i=1; i<=3; i++ ))
-do
-    curl "https://www.baidu.com" > nul
+    echo $i
+    curl -X POST -d "$QQMUSICONEDAYTIME_DATA"
 done
