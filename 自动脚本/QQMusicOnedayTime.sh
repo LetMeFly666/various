91da240669a7e9beb3e5e0eb2cb6c6660dc484d0
LetMeFly666 <814114971@qq.com>
2023-08-24 23:20:04 +0800

    not on push and 90times is enough

--- 自动脚本/QQMusicOnedayTime.sh
+++ 自动脚本/QQMusicOnedayTime.sh
@@ -2,11 +2,11 @@
  # @Author: LetMeFly
  # @Date: 2023-08-24 23:01:18
  # @LastEditors: LetMeFly
- # @LastEditTime: 2023-08-24 23:14:42
+ # @LastEditTime: 2023-08-24 23:20:02
 ### 
 #!/bin/bash
 
-for (( i=1; i<=200; i++ ))
+for (( i=1; i<=90; i++ ))
 do
     echo $i
     curl -X POST -d "$QQMUSICONEDAYTIME_DATA" "https://stat.y.qq.com/pc/fcgi-bin/reportmus.fcg?pcachetime=1692368054" > nul
