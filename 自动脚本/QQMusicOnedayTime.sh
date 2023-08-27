a80d56bf2693d666c1d1af79708fd9454ca6e85f
LetMeFly666 <814114971@qq.com>
2023-08-27 09:10:50 +0800

    每年push一次好了，至少有过这个action

--- 自动脚本/QQMusicOnedayTime.sh
+++ 自动脚本/QQMusicOnedayTime.sh
@@ -2,7 +2,7 @@
  # @Author: LetMeFly
  # @Date: 2023-08-24 23:01:18
  # @LastEditors: LetMeFly
- # @LastEditTime: 2023-08-24 23:20:02
+ # @LastEditTime: 2023-08-27 09:10:12
 ### 
 #!/bin/bash
 
@@ -10,4 +10,6 @@ for (( i=1; i<=90; i++ ))
 do
     echo $i
     curl -X POST -d "$QQMUSICONEDAYTIME_DATA" "https://stat.y.qq.com/pc/fcgi-bin/reportmus.fcg?pcachetime=1692368054" > nul
+    # curl -X POST -d "$QQMUSICONEDAYTIME_DATA" "https://stat.y.qq.com/pc/fcgi-bin/reportmus.fcg?pcachetime=$(date -d "3 minutes 33 seconds ago" +%s)" > nul  # 也不行诶
+    # curl -X POST -d "$QQMUSICONEDAYTIME_DATA" "https://stat.y.qq.com/pc/fcgi-bin/reportmus.fcg?pcachetime=$(date +%s)" > nul  # 这个也不行
 done
